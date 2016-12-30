80995fbd2fad462aa4c030a38d696ad5c7a20e58
Andreas Rasmussen <andreasrefras@gmail.com>
2016-12-30 23:16:12 +0100

    Added possibility to assign users and articles to courses + better delete methods that actually deletes the data from other places it is referenced

--- src/routes/add.vue
+++ src/routes/add.vue
@@ -10,11 +10,11 @@
           <li v-for="(course, index) in courses">
             <b>{{ course.name }}</b>,
             <span>{{ course.id }}</span>
-            <span class="delete" @click="deleteThis('courses/', course.id)">Delete</span>
+            <span class="delete" @click="deleteCourse(course.id)">Delete</span>
           </li>
         </ul>
 
-        <h3 class="margin-top-4-1">Add course</h3>
+        <h4 class="margin-top-4-1">Add course</h4>
         <form v-on:submit.prevent="addCourse">
           <input class="margin-right-2-1" v-model="course.name" type="text" placeholder="Name">
           <label for="weekday">Weekday:</label>
@@ -41,6 +41,19 @@
             Status of articles: <b v-for="(article, index) in user.articles">{{ article.finished }} </b>
           </li>
         </ul>
+
+        <h4 class="margin-top-3-1">Assign user to course</h4>
+        <form v-on:submit.prevent="assignUserToCourse">
+          <label for="user">User ID:</label>
+          <select class="margin-right-2-1" name="user" v-model="assignUserToCourse.userId">
+            <option v-for="(user, index) in users">{{ user.id }}</option>
+          </select>
+          <label for="course">Course ID:</label>
+          <select class="margin-right-2-1" name="course" v-model="assignUserToCourse.courseId">
+            <option v-for="(course, index) in courses">{{ course.id }}</option>
+          </select>
+          <button type="submit">Assign user</button>
+        </form>
       </div>
     </grid-block>
 
@@ -50,10 +63,13 @@
       <div class="span-12">
         <h1>Articles</h1>
         <ul class="list-unstyled border-bottom" v-for="(article, index) in articles">
-          <li><b>{{ article.title }}</b>, {{ article.author }} ({{ article.year }}) <span class="delete" @click="deleteThis('articles/', article.id)">Delete</span></li>
+          <li>
+            <b>{{ article.title }}</b>, {{ article.id }}, {{ article.author }} ({{ article.year }})
+            <span class="delete" @click="deleteArticle(article.id)">Delete</span>
+          </li>
         </ul>
 
-        <h3 class="margin-top-4-1">Add article</h3>
+        <h4 class="margin-top-3-1">Add article</h4>
         <form v-on:submit.prevent="addArticle">
           <input class="margin-right-2-1" v-model="article.title" type="text" placeholder="Title">
           <input class="margin-right-2-1" v-model="article.author" type="text" placeholder="Author">
@@ -61,6 +77,19 @@
           <input class="margin-right-2-1" v-model="article.pageFrom" type="text" placeholder="Page from">
           <input class="margin-right-2-1" v-model="article.pageTo" type="text" placeholder="Page to">
           <button type="submit">Add article</button>
+        </form>
+
+        <h4 class="margin-top-3-1">Assign article to course</h4>
+        <form v-on:submit.prevent="assignArticleToCourse">
+          <label for="article">Article ID:</label>
+          <select class="margin-right-2-1" name="article" v-model="assignArticleToCourse.articleId">
+            <option v-for="(article, index) in articles">{{ article.id }}</option>
+          </select>
+          <label for="course">Course ID:</label>
+          <select class="margin-right-2-1" name="course" v-model="assignArticleToCourse.courseId">
+            <option v-for="(course, index) in courses">{{ course.id }}</option>
+          </select>
+          <button type="submit">Assign article</button>
         </form>
       </div>
     </grid-block>
@@ -92,6 +121,14 @@
           year: null,
           pageFrom: null,
           pageTo: null
+        },
+        assignUserToCourse: {
+          userId: null,
+          courseId: null
+        },
+        assignArticleToCourse: {
+          articleId: null,
+          courseId: null
         }
       }
     },
@@ -138,31 +175,101 @@
         })
       },
       addCourse() {
-        this.databaseRef.ref('courses/').push({
-          name: this.course.name,
-          weekday: this.course.weekday
-        })
-        this.course.name = ''
-        this.course.weekday = ''
+        if ( this.course.name != '' ) {
+          this.databaseRef.ref('courses/').push({
+            name: this.course.name,
+            weekday: this.course.weekday
+          })
+          this.course.name = ''
+          this.course.weekday = ''
+        }
+      },
+      deleteCourse(id) {
+        if ( confirm('Are you sure?') ) {
+          // Delete from courses ref
+          this.databaseRef.ref('courses/' + id).remove()
+          // Delete it from all articles in which the course is referenced
+          this.databaseRef.ref('articles/').on('value', (snapshot) => {
+            const articleObjs = snapshot.val()
+            for (let articleObj in articleObjs) {
+              for (let courseId in articleObjs[articleObj].courses) {
+                if ( courseId === id ) {
+                  this.databaseRef.ref('articles/' + articleObj + '/courses/' + courseId ).remove()
+                }
+              }
+            }
+          }),
+          // Delete it from all users in which the course is referenced
+          this.databaseRef.ref('users/').on('value', (snapshot) => {
+            const userObjs = snapshot.val()
+            for (let userObj in userObjs) {
+              for (let courseId in userObjs[userObj].courses) {
+                if ( courseId === id ) {
+                  this.databaseRef.ref('users/' + userObj + '/courses/' + courseId ).remove()
+                }
+              }
+            }
+          })
+        }
       },
       addArticle() {
-        this.databaseRef.ref('articles/').push({
-          title: this.article.title,
-          author: this.article.author,
-          year: this.article.year,
-          pageFrom: this.article.pageFrom,
-          pageTo: this.article.pageTo
-        })
-        this.article.title = ''
-        this.article.author = ''
-        this.article.year = ''
-        this.article.pageFrom = ''
-        this.article.pageTo = ''
+        if ( this.article.title != '' ) {
+          this.databaseRef.ref('articles/').push({
+            title: this.article.title,
+            author: this.article.author,
+            year: this.article.year,
+            pageFrom: this.article.pageFrom,
+            pageTo: this.article.pageTo
+          })
+          this.article.title = ''
+          this.article.author = ''
+          this.article.year = ''
+          this.article.pageFrom = ''
+          this.article.pageTo = ''
+        }
       },
-      deleteThis(databaseRef, id) {
-        console.log('test');
+      deleteArticle(id) {
         if ( confirm('Are you sure?') ) {
-          this.databaseRef.ref(databaseRef + id).remove()
+          // Delete from articles ref
+          this.databaseRef.ref('articles/' + id).remove()
+          // Delete it from all courses in which the article is referenced
+          this.databaseRef.ref('courses/').on('value', (snapshot) => {
+            const courseObjs = snapshot.val()
+            for (let courseObj in courseObjs) {
+              for (let articleId in courseObjs[courseObj].articles) {
+                if ( articleId === id ) {
+                  this.databaseRef.ref('courses/' + courseObj + '/articles/' + articleId ).remove()
+                }
+              }
+            }
+          }),
+          // Delete it from all users in which the article is referenced
+          this.databaseRef.ref('users/').on('value', (snapshot) => {
+            const userObjs = snapshot.val()
+            for (let userObj in userObjs) {
+              for (let articleId in userObjs[userObj].articles) {
+                if ( articleId === id ) {
+                  this.databaseRef.ref('users/' + userObj + '/articles/' + articleId ).remove()
+                }
+              }
+            }
+          })
+        }
+      },
+      assignUserToCourse() {
+        if (confirm('Assign ' + this.assignUserToCourse.userId + ' to ' + this.assignUserToCourse.courseId + '?') ) {
+          this.databaseRef.ref('courses/' + this.assignUserToCourse.courseId + '/students/' + this.assignUserToCourse.userId).set(true)
+          this.databaseRef.ref('users/' + this.assignUserToCourse.userId + '/courses/' + this.assignUserToCourse.courseId).set(true)
+          this.assignUserToCourse.userId = ''
+          this.assignUserToCourse.courseId = ''
+        }
+      },
+      assignArticleToCourse() {
+        if (confirm('Assign ' + this.assignArticleToCourse.articleId + ' to ' + this.assignArticleToCourse.courseId + '?') ) {
+          this.databaseRef.ref('courses/' + this.assignArticleToCourse.courseId + '/articles/' + this.assignArticleToCourse.articleId).set(true)
+          this.databaseRef.ref('articles/' + this.assignArticleToCourse.articleId + '/courses/' + this.assignArticleToCourse.courseId).set(true)
+          this.assignArticleToCourse.articleId = ''
+          this.assignArticleToCourse.courseId = ''
         }
       }
     }
