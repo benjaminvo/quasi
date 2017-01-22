27b266e3454ba78c886057f8799e46165140f88d
Andreas Rasmussen <andreasrefras@gmail.com>
2017-01-22 15:43:01 +0100

    Wrapped main.js in window load listener to avoid 'Can't find element: #app'-message

--- src/main.js
+++ src/main.js
@@ -5,54 +5,56 @@ import { routes } from './routes'
 import firebase from 'firebase'
 import { fireInit } from './auth/fireInit'
 
-Vue.use(VueRouter)
+window.addEventListener('load', function () {
+  Vue.use(VueRouter)
 
-const router = new VueRouter({
-  routes,
-  linkActiveClass: 'active',
-  scrollBehavior (to, from, savedPosition) {
-    return { x: 0, y: 0 }
-  },
-  mode: 'history' // TODO: Remember to set up the server to ALWAYS serve the index.html file when this history-mode is on (to avoid hashes in the url)
-})
+  const router = new VueRouter({
+    routes,
+    linkActiveClass: 'active',
+    scrollBehavior (to, from, savedPosition) {
+      return { x: 0, y: 0 }
+    },
+    mode: 'history' // TODO: Remember to set up the server to ALWAYS serve the index.html file when this history-mode is on (to avoid hashes in the url)
+  })
 
-// Check if route requires authentication
-router.beforeEach( (to, from, next) => {
-  if (to.meta.auth && !authenticated) {
-    next({ path: 'auth.html' })
-  } else {
-    next()
+  // Check if route requires authentication
+  router.beforeEach( (to, from, next) => {
+    if (to.meta.auth && !authenticated) {
+      next({ path: 'auth.html' })
+    } else {
+      next()
+    }
+  })
+
+  fireInit(fireStatus)
+  let authenticated = false
+
+  function fireStatus(loggedIn, user) {
+    if (loggedIn) {
+
+      authenticated = true
+
+      const Quasi = Vue.extend({
+        components: { 'app': App },
+        data () {
+          return {
+            authenticated: authenticated,
+            currentUser: user,
+            database: firebase.database()
+          }
+        },
+        template: '<app :database="database" :currentUser="currentUser" :authenticated="authenticated" />'
+      })
+
+      new Vue({
+        el: '#app',
+        router,
+        render: h => h(Quasi)
+      })
+
+    } else {
+    	authenticated = false
+      window.location.href = 'auth.html'
+    }
   }
 })
-
-fireInit(fireStatus)
-let authenticated = false
-
-function fireStatus(loggedIn, user) {
-  if (loggedIn) {
-
-    authenticated = true
-
-    const Quasi = Vue.extend({
-      components: { 'app': App },
-      data () {
-        return {
-          authenticated: authenticated,
-          currentUser: user,
-          database: firebase.database()
-        }
-      },
-      template: '<app :database="database" :currentUser="currentUser" :authenticated="authenticated" />'
-    })
-
-    new Vue({
-      el: '#app',
-      router,
-      render: h => h(Quasi)
-    })
-
-  } else {
-  	authenticated = false
-    window.location.href = 'auth.html'
-  }
-}
