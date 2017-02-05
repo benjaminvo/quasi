73808c9f92a882d77e1a9ea2a99f7854d35f0dbc
Benjamin Ottensten <benjamin@ungtriumf.dk>
2017-02-05 14:47:17 +0100

    Add Intercom tracking of (un)finished texts

--- src/utils/toggleArticleFinished.js
+++ src/utils/toggleArticleFinished.js
@@ -6,6 +6,10 @@ export const toggleArticleFinished = {
     toggleArticleFinished(articleId, articleTitle, userId, userName, modalToShow, anonymous = null) {
       const articleFinishedByPath = 'articles/' + articleId + '/finishedBy/' + userId
       const articleFinishedPath = 'users/' + userId + '/articlesFinished/' + articleId
+      const intercomMetadata = {
+        text_title: articleTitle,
+        text_id: articleId
+      }
 
       this.databaseRef.ref(articleFinishedPath).once( 'value', (snapshot) => {
         const data = snapshot.val()
@@ -17,9 +21,14 @@ export const toggleArticleFinished = {
 
           this.notification('articleFinished', articleId, articleTitle, userId, userName, null, anonymous)
 
+          window.Intercom( 'trackEvent', 'text_finished', intercomMetadata )
+          window.Intercom( 'update' )
         } else {
           this.databaseRef.ref(articleFinishedPath).set(false)
           this.databaseRef.ref(articleFinishedByPath).set(false)
+
+          window.Intercom( 'trackEvent', 'text_unfinished', intercomMetadata )
+          window.Intercom( 'update' )
         }
       })
     }
